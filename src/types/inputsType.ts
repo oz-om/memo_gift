237eb786598d5d0c5023770641e6b5cd36a4835a
0z-om <ozyoom@gmail.com>
2024-01-23 13:23:06 +0100

    style:  chosed_image as  global compnent

--- src/types/inputsType.ts
+++
@@ -1,8 +0,0 @@
-export type inputProps = {
-  name?: string;
-  type?: string;
-  placeholder?: string;
-  className?: string;
-  setValue: (fieldType: string, value: any) => void;
-  reset: boolean;
-};
