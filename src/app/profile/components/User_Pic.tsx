77422c18be82b4e541c97b3bc0b5b60a5168313b
0z-om <ozyoom@gmail.com>
2024-05-12 14:39:58 +0100

    feat: update user pic. chnage component name mistake. implement update user pic functionality.

---
+++ src/app/profile/components/User_Pic.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { UPLOAD_URL, confirmUploadImages, toastStyles, uploadImage } from "@/utils";
+import Image from "next/image";
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import { updateUserPick } from "../actions";
+
+export default function User_Pic({ userId, user_pic }: { userId: string; user_pic: string }) {
+  const [insertedImage, setInsertedImage] = useState<{ file: File | undefined; url: string }>({
+    file: undefined,
+    url: user_pic,
+  });
+  const [imageUploadSession, setImageUploadSession] = useState<string | null>(null);
+  const [isUpdated, setIsUpdated] = useState(false);
+  function pickPreview({ currentTarget: fileInput }: React.ChangeEvent<HTMLInputElement>) {
+    const Image = fileInput.files?.[0];
+    if (!Image) return;
+    setInsertedImage({
+      url: URL.createObjectURL(Image),
+      file: Image,
+    });
+    if (!imageUploadSession) {
+      fetch(UPLOAD_URL).then(async (res) => {
+        const uploadSession: { init: boolean; sessionID: string } | undefined = await res.json();
+        if (!uploadSession?.init) return;
+        console.log(uploadSession.sessionID);
+
+        setImageUploadSession(uploadSession.sessionID);
+        setIsUpdated(true);
+      });
+    }
+  }
+  async function updateProfilePick() {
+    const alert = toast;
+    if (!insertedImage.file || !imageUploadSession) return alert.error("please chose an image to upload!", { style: toastStyles });
+    alert.loading("uploading...", { style: toastStyles });
+    const imageId = new Date().getTime();
+    const imageName = insertedImage.file.name;
+    await uploadImage(insertedImage.file, `${imageId}`, imageUploadSession, "user", async (err, res) => {
+      alert.remove();
+      if (err || !res.upload) {
+        console.log(err);
+        return alert.error("something went wrong during uploading!, please try again");
+      }
+      console.log(res);
+      const confirmUploadRes = await confirmUploadImages([{ name: `upat_${res.id}_${imageName}` }], "user");
+      if (!confirmUploadRes.confirmation) {
+        return alert.error(confirmUploadRes.error, { style: toastStyles });
+      }
+
+      const updatePic = await updateUserPick(userId, `${UPLOAD_URL}/images/${confirmUploadRes.uploads[0]}`);
+      if (!updatePic.success) return alert.error(updatePic.error, { style: toastStyles });
+      alert.success("done", { style: toastStyles });
+    });
+  }
+
+  return (
+    <div className='container flex gap-x-4 items-center'>
+      <figure className='overflow-hidden rounded-full border-teal-700 border-2 size-20 shrink-0'>
+        <Image src={insertedImage.url} alt={"user profile"} width={240} height={120} />
+      </figure>
+      <div className='change_pick bg-slate-100 text-teal-500 rounded px-2 flex gap-x-2 items-center'>
+        <input onChange={pickPreview} type='file' name='user_pick' id='userPick' hidden className='hidden' />
+        <label htmlFor='userPick' className='cursor-pointer text-sm'>
+          change pick
+        </label>
+        {isUpdated && (
+          <button onClick={updateProfilePick} className='text-teal-400 border border-teal-400 bg-slate-300 rounded px-2'>
+            save
+          </button>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 import Profile_nav from "./components/Profile_nav";
 import User_Addresses from "./components/User_Addresses";
 import User_Details from "./components/User_Details";
-import User_Pick from "./components/User_Pick";
+import User_Pic from "./components/User_Pic";
 import User_ResetPass from "./components/User_ResetPass";
 
 export default async function Profile_Page() {
@@ -42,7 +42,7 @@ export default async function Profile_Page() {
       <section className='user_info sm:container sm:grid sm:grid-cols-2 md:grid-cols-3'>
         <User_Details userDetails={userDetails} />
         <div className='user_pick_address_wrapper border-y sm:border-y-0 py-2 my-5 relative sm:my-0 before:absolute before:top-0 before:left-1 before:h-full before:border-l-2  before:border-dashed sm:before:content-[""] after:absolute after:top-0 after:right-1 after:h-full after:border-l-2  after:border-dashed after:content-[""] after:hidden md:after:block'>
-          <User_Pick />
+          <User_Pic userId={user.id} user_pic={user.profile_pic} />
           <User_Addresses session={userSession} addresses={address} />
         </div>
         <User_ResetPass />
