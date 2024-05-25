93c20451aacc065aa956826c012cd533c50dd651
0z-om <ozyoom@gmail.com>
2024-05-25 23:07:10 +0100

    style: call hook unconditionally.

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,8 +9,8 @@ import BuiltNewCustomGift from "../home/components/client/BuiltNewCustomGift";
 
 export default function Header({ children }: { children: React.ReactNode }) {
   const path = usePathname();
-  if (/^\/dashboard(?:[\/?].*)?$/.test(path)) return <></>;
   const { data: login } = useSession();
+  if (/^\/dashboard(?:[\/?].*)?$/.test(path)) return <></>;
   return (
     <header className={"" + (path == "/" && "header_wrapper relative  text-white  bg-center bg-cover pb-16")}>
       {path == "/" && <div className='header__layer absolute left-0 top-0 w-full h-full bg-black/20'></div>}
